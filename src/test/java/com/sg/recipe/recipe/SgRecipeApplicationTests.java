d635ad23c8adb325daf8ad7025f8b1eed90347f7
arun k <kumararun@virtusa.com>
2020-01-12 10:23:34 +0530

    added logger in test file

--- src/test/java/com/sg/recipe/recipe/SgRecipeApplicationTests.java
+++ src/test/java/com/sg/recipe/recipe/SgRecipeApplicationTests.java
@@ -1,13 +1,16 @@
 package com.sg.recipe.recipe;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SgRecipeApplicationTests {
 
     @Test
     void contextLoads() {
+        log.debug("context loaded successfully");
     }
 
 }
